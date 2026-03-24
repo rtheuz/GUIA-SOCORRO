b670c28d32bb43deb88ca392c8a3379a6a3a5df9
Matheus de Souza Rodrigues <matheus.761730@graduacao.fsa.br>
2026-03-24 16:18:33 -0300

    Add files via upload

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/GUIA-SOCORRO/sw.js', { scope: '/GUIA-SOCORRO/' })})}
