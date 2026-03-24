168e0b3b5cbc1a0274f7512bfe712dd7b8e5134f
Matheus de Souza Rodrigues <matheus.761730@graduacao.fsa.br>
2026-03-24 15:37:16 -0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'guia-socorro-v1';
+
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request)
+        .then((response) => {
+          if (response && response.status === 200) {
+            const clone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          }
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || fetchPromise;
+    })
+  );
+});
